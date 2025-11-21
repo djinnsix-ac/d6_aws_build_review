958955987538c4098a3e0f7fdb6752e727b46195
acsecgit <allen.clark@acsecurityconsulting.co.uk>
2025-11-21 07:14:07 +0000

    fix: Optimize table column widths and uniform fonts v2.13.14

--- aws_security_assessment_app.py
+++ aws_security_assessment_app.py
@@ -37,7 +37,7 @@ import tempfile
 # ============================================================================
 COLLECTION_SCRIPT = "aws_build_review-v2.3.3.py"
 VERIFICATION_SCRIPT = "aws_build_verification-v2.5.7.py"
-REPORT_SCRIPT = "generate_html_report-v2.13.13.py"
+REPORT_SCRIPT = "generate_html_report-v2.13.14.py"
 
 APP_VERSION = "1.0.2"
 APP_NAME = "AWS Security Assessment Tool"
