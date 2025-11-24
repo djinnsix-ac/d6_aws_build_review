9c5b119f20bc6070dd7309dc42aadfdf0e6e98a6
acsecgit <allen.clark@acsecurityconsulting.co.uk>
2025-11-24 14:49:32 +0000

    Fix Update: Added Djinn Six Branding & Added CIS Control Descriptions

--- aws_security_assessment_app.py
+++ aws_security_assessment_app.py
@@ -37,7 +37,7 @@ import tempfile
 # ============================================================================
 COLLECTION_SCRIPT = "aws_build_review-v2.4.1.py"
 VERIFICATION_SCRIPT = "aws_build_verification-v2.7.1.py"
-REPORT_SCRIPT = "generate_html_report-v2.16.0.py"
+REPORT_SCRIPT = "generate_html_report-v2.16.1.py"
 
 APP_VERSION = "1.0.2"
 APP_NAME = "AWS Security Assessment Tool"
